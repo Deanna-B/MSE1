130369a5975591ab9f96cd9f7f1384172f0e8023
Deanna-B <52015314+Deanna-B@users.noreply.github.com>
2019-08-12 14:22:21 +1200

    Add project files.

---
+++ MSE1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSE1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
